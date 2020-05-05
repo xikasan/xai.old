703c10ef08bb923d43bd6c719a5b798dd655aace
xikasan <xika0907@gmail.com>
2020-05-05 22:16:05 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='xai',
+    version='0.2.1',
+    description="xikasan's ai library",
+    long_description=readme,
+    author='xikasan',
+    # author_email='',
+    url='https://github.com/xikasan/xai',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
