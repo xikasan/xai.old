6d7bc02ec3c2323ea34bb764e4d728b70268775f
xikasan <xika0907@gmail.com>
2020-06-05 14:27:51 +0900

    add import

--- xai/__init__.py
+++ xai/__init__.py
@@ -1,1 +1,9 @@
 # coding: utf-8
+
+from .algorithms.ddpg3 import *
+from .algorithms.dqn import *
+
+from .utils.buffer import *
+from .utils.order import *
+from .utils.update import *
+from .utils.space import *
